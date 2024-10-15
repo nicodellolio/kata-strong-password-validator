d573bb891cef26f43e41df092f38941054f53a05
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-10-15 20:29:08 +0200

    add special character validation
    In addition fixed the for loop and the iteration of the input value

--- script/main.js
+++ script/main.js
@@ -2,54 +2,83 @@ const input = document.getElementById("password");
 const lengthVal = document.getElementById("lengthVal");
 const lowerVal = document.getElementById("lowerVal")
 const numVal = document.getElementById("numVal")
+const specCharVal = document.getElementById("specCharVal")
 
 input.addEventListener('input', function lengthCheck(e) {
-    if (input.value.length >= 5 && input.value.length <= 12) {
-        lengthVal.textContent = "✅ Length between 5 and 12";
+    if (input.value.length >= 6 && input.value.length <= 24) {
+        lengthVal.textContent = "✅ Length between 6 and 24";
     }
     else {
-        lengthVal.textContent = "❌ Length between 5 and 12";
+        lengthVal.textContent = "❌ Length between 6 and 24";
     }
 });
 
-input.addEventListener('input', function lowerDigit(e) {
+input.addEventListener('input', function lowerDigitCheck(e) {
 
     const inputValue = input.value;
-    const inputValueArray = inputValue.split("")
-    const digit = e.data
+    let foundLowerCase = false
 
-    if (isNaN(digit * 1)) {
+    for (let i = 0; i < inputValue.length; i++) {
+        const singleDigit = inputValue[i];
 
-        if (digit == digit.toLowerCase()) {
-            lowerVal.textContent = "✅ At least one lower case"
+        if (isNaN(singleDigit * 1) && singleDigit == singleDigit.toLowerCase()) {
+
+            foundLowerCase = true;
+            break;
+            
         }
+    }
+    
+    if (foundLowerCase) {
+        lowerVal.textContent = "✅ Min. 1 lower case"
+    }
+    else{
+        lowerVal.textContent = "❌ Min. 1 lower case"
+    }
 
-        //TENTATIVO DI CICLARE ALL'INTERNO DEL VALUE DELL'INPUT PER CAMBIARE IL VALORE DI isLower IN CASO DI ASSENZA DI LETTERE MAIUSCOLE (DOPO UN BACKSPACE)
-        // for (let i = 0; i < inputValueArray.length; i++) {
-        //     let singleDigit = inputValueArray[i];
 
-        //     let isLower = false
-        //     if (singleDigit == singleDigit.toLowerCase()) {
-        //         isLower = true
-        //     }
+});
 
-        //     console.log(isLower);
-        //     return isLower;
-        // }
+input.addEventListener('input', function numberCheck(e) {
+    const inputValue = input.value;
+    let isNumber = false
+
+    for (let i = 0; i < inputValue.length; i++) {
+        const singleDigit = inputValue[i];
+
+        if (!isNaN(singleDigit * 1)) {
+            isNumber = true
+            break
+        }
+    }
+
+    if (isNumber) {
+        numVal.textContent = "✅ Min. 1 number"
+    }
+    else {
+        numVal.textContent = "❌ Min. 1 number"
     }
 
 });
 
-input.addEventListener('input', function lowerDigit(e) {
-
+input.addEventListener('input', function specCharCheck() {
     const inputValue = input.value;
-    const inputValueArray = inputValue.split("")
-    const digit = e.data
-    console.log(typeof digit);
+    const specCharArray = ["!", "@", "#", "$", "%", "*", "+", "<", ">"];
+    let foundSpecialChar = false;
 
-    if (!isNaN(digit * 1)) {
-        numVal.textContent = "✅ At least one number"
+    for (let i = 0; i < inputValue.length; i++) {
+        const singleDigit = inputValue[i];
+
+        if (specCharArray.includes(singleDigit)) {
+            foundSpecialChar = true;
+            break
+        }
     }
 
+    if (foundSpecialChar) {
+        specCharVal.textContent = "✅ Min. 1 special character";
+    } else {
+        specCharVal.textContent = "❌ Min. 1 special character (!, @, #, $, %, *, +, <, >)";
+    }
 });
 
