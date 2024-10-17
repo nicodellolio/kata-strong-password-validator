ad4d8df524a092ae3822110f7029401f51686787
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-10-17 20:18:14 +0200

    add class to turn text green when condition is true

--- script/main.js
+++ script/main.js
@@ -10,6 +10,8 @@ let isNumber = false;
 let foundSpecialChar = false;
 
 input.addEventListener('input', function lengthCheck(e) {
+    rightLength = false
+
 
     if (input.value.length >= 6 && input.value.length <= 24) {
         rightLength = true
@@ -20,9 +22,11 @@ input.addEventListener('input', function lengthCheck(e) {
 
     if (rightLength) {
         lengthVal.textContent = "✅ Length between 6 and 24";
+        lengthVal.classList.add("text-success")
     }
     else {
         lengthVal.textContent = "❌ Length between 6 and 24";
+        lengthVal.classList.remove("text-success")
     }
 
 });
@@ -30,6 +34,8 @@ input.addEventListener('input', function lengthCheck(e) {
 input.addEventListener('input', () => {
 
     const inputValue = input.value;
+    foundLowerCase = false;
+
 
     for (let i = 0; i < inputValue.length; i++) {
         const singleDigit = inputValue[i];
@@ -38,21 +44,26 @@ input.addEventListener('input', () => {
         if (/[a-z]/.test(singleDigit)) {
             foundLowerCase = true;
             break;
-
+        }
+        else {
+            foundLowerCase = false;
         }
     }
 
     if (foundLowerCase) {
         lowerVal.textContent = "✅ Min. 1 lower case";
+        lowerVal.classList.add("text-success")
     }
     else {
         lowerVal.textContent = "❌ Min. 1 lower case";
+        lowerVal.classList.remove("text-success")
     }
 
 });
 
 input.addEventListener('input', () => {
     const inputValue = input.value;
+    isNumber = false
 
     for (let i = 0; i < inputValue.length; i++) {
         const singleDigit = inputValue[i];
@@ -65,15 +76,20 @@ input.addEventListener('input', () => {
 
     if (isNumber) {
         numVal.textContent = "✅ Min. 1 number";
+        numVal.classList.add("text-success")
     }
     else {
         numVal.textContent = "❌ Min. 1 number";
+        numVal.classList.remove("text-success")
     }
+
 
 });
 
 input.addEventListener('input', () => {
     const inputValue = input.value;
+    foundSpecialChar = false;
+
     // const specCharArray = ["!", "@", "#", "$", "%", "*", "+", "<", ">"];
 
     for (let i = 0; i < inputValue.length; i++) {
@@ -87,13 +103,17 @@ input.addEventListener('input', () => {
 
     if (foundSpecialChar) {
         specCharVal.textContent = "✅ Min. 1 special character";
+        specCharVal.classList.add("text-success")
     } else {
         specCharVal.textContent = "❌ Min. 1 special character (!, @, #, $, %, *, +, <, >)";
+        specCharVal.classList.remove("text-success")
     }
 
 });
 
 input.addEventListener('input', () => {
+
+    //  LOGIC
     const validatingConditions = [
         rightLength,
         foundLowerCase,
@@ -101,18 +121,54 @@ input.addEventListener('input', () => {
         foundSpecialChar
     ];
 
-    let trueValidatingConditions = []
-
-    trueValidatingConditions.length = 0
-
+    let trueValidatingConditions = [];
 
     for (let i = 0; i < validatingConditions.length; i++) {
         const validatingCondition = validatingConditions[i];
 
-        if (validatingCondition == true) {
-            trueValidatingConditions.length++
+        if (validatingCondition) {  // Se la condizione è true
+            trueValidatingConditions.push(true);
         }
     }
-    console.log(trueValidatingConditions.length);
-});
 
+    // console.log(trueValidatingConditions);
+
+    //DOM MANIPULATION
+    const barDanger = document.getElementById("barDanger");
+    const barWarning = document.getElementById("barWarning");
+    const barOk = document.getElementById("barOk");
+    const barSuccess = document.getElementById("barSuccess");
+
+    switch (trueValidatingConditions.length) {
+        case 1:
+            barDanger.removeAttribute('hidden');
+            barWarning.setAttribute('hidden', '')
+            barOk.setAttribute('hidden', '')
+            barSuccess.setAttribute('hidden', '')
+            break;
+        case 2:
+            barDanger.setAttribute('hidden', '')
+            barWarning.removeAttribute('hidden')
+            barOk.setAttribute('hidden', '')
+            barSuccess.setAttribute('hidden', '')
+            break;
+        case 3:
+            barDanger.setAttribute('hidden', '')
+            barWarning.setAttribute('hidden', '')
+            barOk.removeAttribute('hidden')
+            barSuccess.setAttribute('hidden', '')
+            break;
+        case 4:
+            barDanger.setAttribute('hidden', '')
+            barWarning.setAttribute('hidden', '')
+            barOk.setAttribute('hidden', '')
+            barSuccess.removeAttribute('hidden')
+            break;
+        default:
+            barDanger.setAttribute('hidden', '')
+            barWarning.setAttribute('hidden', '')
+            barOk.setAttribute('hidden', '')
+            barSuccess.setAttribute('hidden', '' )
+            break;  
+        };
+});
